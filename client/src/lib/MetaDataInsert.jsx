ec29015c3c1cf90d4946d4674b4f3a7fd25ee668
Keith Owino <designsolutions1629@gmail.com>
2026-05-11 18:32:18 +0300

    Add Google Search Console verification

--- client/src/lib/MetaDataInsert.jsx
+++ client/src/lib/MetaDataInsert.jsx
@@ -2,26 +2,40 @@ import { Helmet } from "react-helmet-async";
 import { metadata } from "./DynamicData";
 
 const MetaDataInsert = ({ title = metadata.title }) => {
-  let pageTitle =
-    title === metadata.title
-      ? `${title} - ${metadata.author}`
-      : `${title} - ${metadata.title}`;
+	let pageTitle =
+		title === metadata.title
+			? `${title} - ${metadata.author}`
+			: `${title} - ${metadata.title}`;
 
-  return (
-    <Helmet>
-      <title>{pageTitle}</title>
-      <meta
-        name="description"
-        content={`${metadata.title} - Learn as i build template.`}
-      />
-      <meta
-        name="keywords"
-        content="coding, website design, freelancing, software development, ai, llm"
-      />
-      <meta property="og:title" content={pageTitle} />
-      <meta property="og:description" content="Self made template." />
-    </Helmet>
-  );
+	return (
+		<Helmet>
+			<title>{pageTitle}</title>
+			{/* 
+      
+
+      <meta property="og:title" content="Pickaxe & Shovel" />
+<meta
+  property="og:description"
+  content="Developer playground and coding tools."
+/>
+      
+      
+      */}
+			<meta
+				name="description"
+				content={`${metadata.title} is a developer playground for code execution, experiments, and tooling.`}
+			/>
+			<meta
+				name="keywords"
+				content="coding, website design, freelancing, software development, ai, llm"
+			/>
+			<meta property="og:title" content={pageTitle} />
+			<meta
+				property="og:description"
+				content="Developer playground and coding tools."
+			/>
+		</Helmet>
+	);
 };
 
 export default MetaDataInsert;
